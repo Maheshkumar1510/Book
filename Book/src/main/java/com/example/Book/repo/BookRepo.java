9b6824bd5b1e0174f79ec05e4e097a012610b314
Maheshkumar1510 <maheshmaahi151098@gmail.com>
2025-08-27 08:52:23 +0530

    Delete Book directory

--- Book/src/main/java/com/example/Book/repo/BookRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.Book.repo;
-
-import com.example.Book.entity.BookEntity;
-import org.hibernate.sql.ast.tree.expression.JdbcParameter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<BookEntity,Long> {
-}
